ea0b35f5b4ed443eacb427e5de1a7a5a2c0174bd
ihorkup <ihora021099@gmail.com>
2020-01-16 23:08:47 +0200

    update input message & post

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {createRef} from 'react';
 import s from './Dialogs.module.css';
 import Dialog from './Dialog/Dialog';
 import Message from './Message/Message';
@@ -8,13 +8,27 @@ const Dialogs = (props) => {
     let dialogsElements = props.state.dialogs.map(d => <Dialog name={d.name} id={d.id} src={d.src} />);
     let messagesElements = props.state.messages.map(m => <Message message={m.message} id={m.id} />);
 
+    let messageInputRef = React.createRef();
+
+    let newMessage = () => {
+        let textMessage = messageInputRef.current.value;
+        alert(textMessage);
+    }
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
                 {dialogsElements}
             </div>
-            <div className={s.messages}>
-                {messagesElements}
+            <div>
+                <div className={s.messages}>
+                    {messagesElements}
+                </div>
+                <div className={s.areaInputMessage}>
+                    <textarea ref={messageInputRef} className={s.areaInputText}></textarea>
+                    <div>
+                        <button onClick={newMessage} className={s.buttonSendMessage}>Send</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/components/Profile/posts.jsx
+++
@@ -1,9 +0,0 @@
-// let posts = [
-//     { id: 1, message: "It's my first post", likeCount: "15" },
-//     { id: 2, message: "I xfbnxcnbcxknb you", likeCount: "111111" },
-//     { id: 3, message: "How are you?", likeCount: "14" },
-//     { id: 4, message: "How old are you?", likeCount: "4" },
-//     { id: 5, message: "You are very clever", likeCount: "27" }
-// ]
-
-// export default posts;
